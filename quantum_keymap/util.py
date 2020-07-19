13d6a887b5674a124d4fe240007be65219436ae6
aoirohn <aoiro.hn@gmail.com>
2020-07-19 21:08:40 +0900

    add util function to load config

---
+++ quantum_keymap/util.py
@@ -0,0 +1,19 @@
+from types import ModuleType
+
+
+def load_config(module: ModuleType):
+    dic = {}
+
+    for key in [
+        "HAND",
+        "FINGER",
+        "POSITION_COST",
+        "CONSECUTIVE_HAND_COST",
+        "CONSECUTIVE_FINGER_COST",
+        "CONSECUTIVE_KEY_COST",
+        "KEY_ID",
+    ]:
+        if key in vars(module):
+            dic[key] = vars(module)[key]
+
+    return dic
